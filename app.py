2b17f69bd61e4edac59e7fea4b2eddacbb2dce1e
kalnar328 <40544324+kalnar328@users.noreply.github.com>
2021-01-07 14:13:03 +0530

    Rq removed

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask import Flask, request, jsonify, copy_current_request_context
 from image_processing import process_image_less_noise, process_image_for_ocr, get_image_cv, get_image_pil
 from data_extraction import process_ocr
 
-from rq import Queue
-from worker import conn
+# from rq import Queue
+# from worker import conn
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -17,7 +17,6 @@ cors = CORS(app)
 
 #  ?url=
 @app.route('/url', methods=['POST', 'GET'])
-@copy_current_request_context
 def processURL():
     # load image from the url
     image_cv = get_image_cv(request)
@@ -34,7 +33,7 @@ def processURL():
 
     # word correction using knowledge graph
     if result:
-        print("Making API call to knowledge graph...")
+        print("Request directing to knowledge graph...")
         # response = requests.post(url="http://127.0.0.1:3000/corrections", data=result)
         response = requests.post(url="https://diabipal-knowledge-graph.herokuapp.com/corrections", data=result)
         print("Status from the KG API: ", response.status_code)
@@ -59,7 +58,7 @@ def processURL():
             return jsonify({'Error': 'Unable to identify your report. Please try again'})
 
         if out_array:
-            print("Making API call to knowledge graph...")
+            print("Request directing to knowledge graph...")
             # response = requests.post(url="http://127.0.0.1:3000/corrections", data=result)
             response = requests.post(url="https://diabipal-knowledge-graph.herokuapp.com/corrections", data=out_array)
             print("Status code of the response KG API: ", response.status_code)
@@ -73,11 +72,11 @@ def processURL():
             return {'Error'}
 
 
-# create RQ queue
-q = Queue(connection=conn)
-# send jobs to Redis
-result = q.enqueue(processURL, result_ttl=80)
-print(result)
+# # create RQ queue
+# q = Queue(connection=conn)
+# # send jobs to Redis
+# result = q.enqueue(processURL, result_ttl=80)
+# print(result)
 
 if __name__ == '__main__':
     app.run(debug="true")
